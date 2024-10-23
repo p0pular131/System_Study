5aabea4be30f4f8604d911eb218da6244e923f30
p0pular131 <popular131@g.skku.edu>
2024-10-24 00:51:43 +0900

    PS PA1 완료

--- PS/PA1/main.c
+++ PS/PA1/main.c
@@ -11,6 +11,13 @@ float weights[301];
 float P[3];
 int polSize = 0;
 
+int check_zero(float input) {
+    if(input < 0.01 && input > -0.01) {
+        return 1;
+    }
+    return -1;
+}
+
 int update_P() {
     if(polSize < 1) {
         return -1;
@@ -19,10 +26,17 @@ int update_P() {
     for(int i=1;i<=polSize;i++) {
         weightSum += weights[i];
     }
-    
+    P[0] = 0.0f; P[1] = 0.0f; P[2] = 0.0f;
+    for(int i=1;i<=polSize;i++) {
+        P[0] += weights[i] * poly[i][0];
+        P[1] += weights[i] * poly[i][1];
+        P[2] += weights[i] * poly[i][2];
+    }
+    P[0] /= weightSum;
+    P[1] /= weightSum;
+    P[2] /= weightSum;
     return 0;
 }
-
 
 void add_function(float a, float b, float c) {
     poly[polSize+1][2] = a; // 2차항 계수
@@ -32,44 +46,57 @@ void add_function(float a, float b, float c) {
 }
 
 int remove_function(int idx) {
-    if(idx > polSize) {
+    if(idx > polSize || idx < 1) {
         return -1;
     }
     for(int i=idx;i<polSize;i++) {
         // move polynomials
-        poly[idx][0] = poly[idx+1][0];
-        poly[idx][1] = poly[idx+1][1];
-        poly[idx][2] = poly[idx+1][2];
+        poly[i][0] = poly[i+1][0];
+        poly[i][1] = poly[i+1][1];
+        poly[i][2] = poly[i+1][2];
     }
     for(int i=idx;i<polSize;i++) {
         // move weights
-        weights[idx] = weights[idx+1];
+        weights[i] = weights[i+1];
     }
     polSize--;
     return 0;
 }
 
 int set_weight(int idx, float weight) {
-    if(idx > polSize) {
+    if(idx > polSize || idx < 1) {
         return -1;
     }
     weights[idx] = weight;
     return 0;
 }
 
-double evaluate_prime(float x) {
-    double result = 0.0;
-    for(int idx = 0; idx < 3; idx ++) {
-        P[idx] = 0;
-        for(int i=1; i<=polSize; i++) {
-            
-        }
-    }
-    return result;
+float evaluate_prime(float x) {
+    update_P(); 
+    return P[0] + P[1]*x + P[2]*x*x;;
 }
 
 void find_root() {
+    update_P();
 
+    if(check_zero(P[2])==1 && check_zero(P[1])==1) {
+        printf("CONST\n");
+    }
+    else if(check_zero(P[2])==1 && check_zero(P[1])==-1) {
+        printf("%.3f\n", -P[0]/P[1]);
+    }
+    else {
+        float det = P[1]*P[1] - 4*P[2]*P[0];
+        if(check_zero(det)==1) {
+            printf("%.3f\n", -P[1]/(2*P[2]));
+        }
+        else if(det > 0) {
+            printf("%.3f %.3f\n", (-P[1] - sqrt(det))/(2*P[2]), (-P[1] + sqrt(det))/(2*P[2]));
+        }       
+        else {
+            printf("NOROOT\n");
+        }
+    }
 }
 
 int main()
