e188472f0e05c25753a7325e7608f62289360482
popular131 <popular131@g.skku.edu>
2024-11-10 14:51:36 +0900

    PS PA2 avg 작업

--- PS/PA2/main.c
+++ PS/PA2/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#define DUMMY [1,1,1,1,0]
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+
+struct Node {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int coin; // total 아니고 각 cell마다 수집할 코인 개수
+}typedef Node;
 
 int N, M; // grid size, operations
 int grid[101][101]; 
+int gridAvg[101][101];
 int maskSize = 1;
+int totalSum = 0, avg = 0;
+int buff_len = 0;
+Node buff[100];
+
+void printDummy() {
+    printf("1 1 1 1 0\n");
+}
 
 void printAll() {
     int printCnt = 0;
@@ -16,9 +35,24 @@ void printAll() {
         }
     }
     while(printCnt <= M) {
-        printf("1 1 1 1 0\n");
+        printDummy();
         printCnt++;
     }
+}
+
+int collectCoin(Node cell) {
+    int x1 = cell.x1, x2 = cell.x2, y1 = cell.y1, y2 = cell.y2, coin = cell.coin;
+    // 해당 영역에서의 코인들을 수집하여 grid를 update하는 함수
+    for(int i=x1;i<=x2;i++) {
+        for(int j=y1;j<=y2;j++) {
+            grid[i][j] -= coin;
+            // invalid operation -> termiante abnormally
+            if(grid[i][j] < 0) {
+                return -1;
+            }
+        }
+    }
+    return 0;
 }
 
 int main() {
@@ -31,12 +65,28 @@ int main() {
     // -1로 grid 초기화
     for(int i=0;i<101;i++) {
         memset(grid[i],-1,101);
+        memset(gridAvg[i],-1,101);
     }
     // grid에 input값 삽입
     fscanf(fp,"%d %d",&N,&M);
     for(int i=1;i<=N;i++) {
         for(int j=1;j<=N;j++) {
             fscanf(fp,"%d ",&grid[i][j]);
+            totalSum += grid[i][j];
+        }
+    }
+    avg = totalSum / (N*N);
+
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+            if(grid[i][j] >= avg) gridAvg[i][j] = grid[i][j];
+            else gridAvg[i][j] = -1;
+        }
+    }
+
+    for(int i=1;i<=N;i++) {
+        for(int j=1;j<=N;j++) {
+
         }
     }
 
